fbf68fdae301746424e2fe23dd2a30e010846c00
AlexEngine95 <alexengine95@gmail.com>
2025-12-26 21:59:16 +0000

    Update controller-vibration-tester.py
    Implements a limit of the motor speed to 65,535 (16 bit integer limit) as values above these cause the motors to stop under XInput.

--- controller-vibration-tester.py
+++ controller-vibration-tester.py
@@ -25,18 +25,8 @@ while True:
     elif RY < 0.0:
         right_speed -= RY * 100
 
-    LY = 0 if LY < 0 else LY
-    RY = 0 if RY < 0 else RY
-
-    set_vibration(0, abs(left_speed), abs(right_speed))
-
-    print(f'Vibration speeds: LEFT {int(left_speed)}   RIGHT {int(right_speed)}')
-    print(f'Stick values:     LY   {LY}                RY    {RY}')
-
-
     if (button_values['START'] == True and button_values['BACK'] == True):
         sys.exit(0)
-
     elif (button_values['LEFT_SHOULDER'] == True):
         left_speed -= 500
         right_speed -= 500
@@ -47,3 +37,16 @@ while True:
         right_speed = 0
     elif (button_values['LEFT_THUMB'] == True):
         left_speed = 0
+    
+    if left_speed > 65536:    #Clamps values of the motor speed to 65,536 as that is the limit under XInput. Values above these cause the motors to stop.
+        left_speed = 65536
+    if right_speed > 65536:
+        right_speed = 65536
+    
+    LY = 0 if LY < 0 else LY
+    RY = 0 if RY < 0 else RY
+
+    set_vibration(0, abs(left_speed), abs(right_speed))
+
+    print(f'Vibration speeds: LEFT {int(left_speed)}   RIGHT {int(right_speed)}')
+    print(f'Stick values:     LY   {LY}                RY    {RY}')
